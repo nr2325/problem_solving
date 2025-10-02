e5d3c49e7f8987be1a5728021b2d522e450ae949
Naziur Rahman <naziurrahman232@gmail.com>
2025-10-03 00:46:45 +0600

    update

---
+++ 486a.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include <cmath>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::multiprecision;
+using namespace std;
+int main(){
+long  double fn,es,os;
+cpp_int r,fs,ss,fr;
+cin>>fn;
+fs=floor((fn/2));
+ss=floor(((fn/2))+1);
+fr=fs*ss;
+os=round((fn/2))*round((fn/2));
+r=floor((fn/2))*floor(((fn/2))+1);
+cout<<500000000000000*500000000000001;
+return 0;
+}
